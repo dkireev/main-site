da60fd29c8d7d6137eb0d47d5a56a8ada172eac0
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2022-06-17 12:31:35 +0300

    update banner

--- wp-content/themes/nord_theme/header.php
+++ wp-content/themes/nord_theme/header.php
@@ -11,15 +11,11 @@
 
 <body class="flex flex-col min-h-screen text-base bg-gray-100 text-gray-900">
   <header class="flex justify-center">
-    <div class="container flex justify-between items-center p-5 gap-10">
+    <div class="container flex justify-between items-center py-3 px-5 md:py-5 gap-10">
       <a href="/">
-        <img src="/wp-content/themes/nord_theme/assets/images/logo_full.svg" class="hidden md:block h-10 w-auto"
-          alt="logo" />
-        <img src="/wp-content/themes/nord_theme/assets/images/logo_short.svg" class="block md:hidden h-10 w-auto"
-          alt="logo" />
+        <img src="/wp-content/themes/nord_theme/assets/images/logo_full.svg" class="block h-10 w-auto" alt="logo" />
       </a>
-      <a href="https://calendly.com/nordsoftworks/30min" target="_blank" rel="noreferrer" role="button"
-        class="py-3 px-6 rounded-md bg-lime-600 hover:bg-lime-700 active:scale-95 transition-all duration-200 text-white text-sm font-semibold tracking-wider uppercase">Book
-        call</a>
+      <?php $outline = true;
+      include("assets/components/button.php") ?>
     </div>
   </header>
--- wp-content/themes/nord_theme/index.php
+++ wp-content/themes/nord_theme/index.php
@@ -1,9 +1,16 @@
 <?php get_header(); ?>
 <main class="flex flex-col grow">
   <section
-    class="flex justify-center items-center w-full h-[calc(100vh-5.25rem)] max-h-[48rem] px-5 bg-no-repeat bg-cover bg-center"
+    class="flex justify-center items-center w-full md:h-[calc(100vh-5.25rem)] md:max-h-[48rem] px-5 bg-no-repeat bg-cover bg-center"
     style="background-image: url(/wp-content/themes/nord_theme/assets/images/banner.jpeg)">
-    <h1 class="text-3xl font-semibold text-white">Coming soon...</h1>
+    <div class="container flex flex-col gap-10 py-20 md:max-w-2xl md:gap-20 md:items-start text-white">
+      <div class="flex flex-col gap-5 md:gap-10 text-center md:text-start">
+        <h1 class="text-4xl md:text-7xl font-semibold">We create custom Landing Pages</h1>
+        <p class="md:max-w-md">Driven and ambitious Nord Softworks team produce customized Landing Pages that captivate
+          users and compel them to take actions</p>
+      </div>
+      <?php include("assets/components/button.php") ?>
+    </div>
   </section>
   <section class="flex grow"></section>
 </main>
